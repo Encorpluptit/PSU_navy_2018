d3ccf744d29cedf683c9a7bf7ebdf1d6eea1482d
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-17 10:27:34 +0200

    [ADD] #INCLUDE# {essentials.h} Adding header with const and attribute macros.

---
+++ include/essentials.h
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2019
+** any_project
+** File description:
+** Essentials Functions of Lib C.
+*/
+
+//<===============================================>
+//<===============================================>
+// Essentials Defines.
+#ifndef UNUSED
+#define UNUSED __attribute__((unused))
+#endif /* UNUSED */
+
+#ifndef HIDDEN
+#define HIDDEN __attribute__((visibility ("hidden")))
+#endif /* HIDDEN */
+
+#ifndef DESTRUCTOR
+#define DESTRUCTOR __attribute__((destructor))
+#endif /* DESTRUCTOR */
+
+#ifndef CONSTRUCTOR
+#define CONSTRUCTOR __attribute__((constructor))
+#endif /* CONSTRUCTOR */
+
+//<===============================================>
+//<===============================================>
+// Essentials Static Constantes.
+#ifndef _STATICS_CONST_RETURN_
+#define _STATICS_CONST_RETURN_
+#include <stddef.h>
+static const int ERROR = 84;
+static const int NOT_VALID = -42;
+static const int SUCCESS = 0;
+UNUSED static const char *MALLOC_FAILED = NULL;
+
+//<===============================================>
+//<===============================================>
+// Essentials Static Constantes for strcmp/strncmp/strcmp_alpha/..only_alpha.
+static const int S1_LONGER = 1;
+static const int S2_LONGER = 2;
+static const int S1_UPPER = -1;
+static const int S2_UPPER = -2;
+static const int EQUAL = 0;
+
+#endif /* _STATICS_CONST_RETURN_ */
