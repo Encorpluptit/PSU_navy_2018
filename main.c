33e9201250a10c2a2f23bfdf4ccbf54ba3cc7b28
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-17 10:19:25 +0200

    [UPDATE] #MAIN# {main.c} Checking numbers of arguments and testing parser fonction.

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 /*
 ** EPITECH PROJECT, 2019
-** Project.
+** Navy.
 ** File description:
-** Project test main.
+** Main for navy project, error gestion and lauching navy.
 */
 
-int main(void)
+#include "essentials.h"
+#include "navy.h"
+
+int main(int ac, char **av, char **env)
 {
-    return (0);
+    if (ac < 2 || ac > 4 || !env)
+        return 84;
+    char **test = parser_entry(av[1]);
+    return SUCCESS;
 }
