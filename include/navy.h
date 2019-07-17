ba6c4a42216687be6cde5d7437eaebe79949fd45
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-17 10:31:16 +0200

    [ADD] #INCLUDE# {navy.h} Adding Navy header, for the entire project.

---
+++ include/navy.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** Navy.
+** File description:
+** Header for the entire project.
+*/
+
+#ifndef _NAVY_H_
+#define _NAVY_H_
+
+//<===============================================>
+//<===============================================>
+// Project functions prototypes.
+
+//<===============================================>
+// map_parser folder.
+char **parser_entry(char *map_path);
+
+#endif /* _NAVY_H_ */
