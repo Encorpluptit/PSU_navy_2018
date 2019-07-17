4eeccb222a05eed59f56e1a7e744c9496c9e49b1
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-17 10:29:40 +0200

    [ADD] #INLCUDE# {map_parser.h} Adding map_parser header for header compartimentation.

---
+++ include/map_parser.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** Navy.
+** File description:
+** header for map_parser module.
+*/
+
+#ifndef _MAP_PARSER_
+#define _MAP_PARSER_
+
+#include <inttypes.h>
+
+static const int8_t FILE_ERROR = -1;
+
+char *get_mainstr(char const *map_path);
+
+#endif /* _MAP_PARSER_ */
