d53049f2d817273deafb4be969432986ec392368
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-17 10:28:33 +0200

    [ADD] #INCLUDE# {get_next_line.h} Adding header for GNL library.

---
+++ include/get_next_line.h
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2019
+** Get_Next_Line
+** File description:
+** Re-Write the getnextline fct.
+*/
+
+//<===============================================>
+//<===============================================>
+// Essentials Static Constantes.
+#ifndef READ_SIZE
+#define READ_SIZE (20)
+#endif
+
+#ifndef __GET_NEXT_LINE_H__
+#define __GET_NEXT_LINE_H__
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+static const int FAILED_MALLOC = -1;
+static const int VALID_MALLOC = 42;
+static const int END_LINE = 1;
+static const int END = 0;
+
+#endif/* __GET_NEXT_LINE_H__ */
