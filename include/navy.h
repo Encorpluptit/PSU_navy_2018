7e1325eb9ca01e3de152376b36fb8560dc83be34
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-19 22:39:28 +0200

    [UPDATE] #INCLUDE# {NAvy.h} Updating navy header with functions protypes for LIB and player entries.

--- include/navy.h
+++ include/navy.h
@@ -13,7 +13,20 @@
 // Project functions prototypes.
 
 //<===============================================>
+// LIB prototypes.
+int my_str_isnum(char const *str);
+int my_atoi(char const * const str);
+
+//<===============================================>
 // map_parser folder.
 char **parser_entry(char *map_path);
+
+//<===============================================>
+// player_one_entry.c.
+int player_one_entry(char *map_path);
+
+//<===============================================>
+// player_two_entry.c.
+int player_two_entry(char *str_pid, char *map_path);
 
 #endif /* _NAVY_H_ */
