7762eb4363baf663961f0dab68b345c655a2539f
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-22 22:43:27 +0200

    [UPDATE] #SRC/INCLUDE# {global.c} Adding singleton function for stop sending signal.

--- src/signals/global.c
+++ src/signals/global.c
@@ -13,9 +13,22 @@ int signal_info(uint32_t value, global_flag_t flag)
 
     if (flag == SET)
         value_save = value;
-    if (flag == RETURN)
+    if (flag == GET)
         return value_save;
     if (flag == RESET)
         value_save = 0;
     return value_save;
+}
+
+bool stop_signal(global_flag_t flag)
+{
+    static bool stop_save = false;
+
+    if (flag == SET)
+        stop_save = true;
+    if (flag == GET)
+        return stop_save;
+    if (flag == RESET)
+        stop_save = false;
+    return stop_save;
 }
