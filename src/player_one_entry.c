a72c1d79004e801c700874c77c70169e894331ee
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-22 15:03:00 +0200

    [UPDATE] #SRC# {player_one_entry.c} {player_two _entry.c} Update for initial connexion.

--- src/player_one_entry.c
+++ src/player_one_entry.c
@@ -8,6 +8,7 @@
 #include <signal.h>
 #include "essentials.h"
 #include "navy.h"
+#include "global.h"
 #include "printf.h"
 
 static int wait_for_player_two(void);
@@ -16,33 +17,47 @@ static void make_connection(int signum, siginfo_t *si, void *ucontext);
 int player_one_entry(char *map_path)
 {
     char **map = parser_entry(map_path);
+    pid_t pid = getpid();
+    pid_t enemy_pid = -1;
+    /* navy_t game; */
 
     if (!map)
         return ERROR;
-    pid_t pid = getpid();
     my_printf("my_pid:  %d\n", pid);
-    wait_for_player_two();
+    enemy_pid = wait_for_player_two();
+    my_printf("%d\n", enemy_pid);
+    /* if (!(game = fill_game_info(map, pid, GET_GLOBAL))) */
+    /*     return ERROR; */
+    /* return (launch_game(game, flag_player)); */
     return SUCCESS;
 }
 
 static int wait_for_player_two(void)
 {
     struct sigaction sa;
+    pid_t enemy_pid = -1;
 
     sa.sa_sigaction = make_connection;
     sa.sa_flags = SA_SIGINFO;
-//    sigemptyset(&sa.sa_mask);
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGUSR2, &sa, NULL);
-    for (int i = 1; i;) {
+    my_printf("waiting for enemy connection...\n\n");
+    while (1) {
         pause();
-        i = 0;
+        if ((enemy_pid = GET_GLOBAL)) {
+            kill(enemy_pid, SIGUSR1);
+            break;
+        }
     }
+    return enemy_pid;
 }
 
 static void make_connection(int signum, siginfo_t *si, void *ucontext)
 {
     if (signum == SIGUSR2) {
-        my_printf("Signal recu\n");
+        my_printf("enemy connected\n");
+        SET_GLOBAL(si->si_pid);
         return;
     }
 }
