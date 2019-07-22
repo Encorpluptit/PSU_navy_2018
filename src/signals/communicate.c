df8b4f7f3a353979b4900c51dd129229632c3efb
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-23 00:43:14 +0200

    [UPDATE] #SRC/SIGNALS# Adding send_info et receive info.

---
+++ src/signals/communicate.c
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2019
+** Navy.
+** File description:
+** Source file for communication by signals (send/receive).
+*/
+
+#include <signal.h>
+#include "global.h"
+
+#include "printf.h"
+
+static void catch_signal(int signum);
+
+int send_info(uint32_t enemy_pid, uint8_t info)
+{
+    if (!info)
+        return (my_printf("error in send_info fct (nothing to send)\n"));
+    for (; info; --info) {
+        usleep(500);
+        kill(enemy_pid, SIGUSR1);
+    }
+    usleep(500);
+    kill(enemy_pid, SIGUSR2);
+}
+
+int receive_info(void)
+{
+    siga_t sa = (siga_t) {.sa_sigaction = catch_signal};
+
+    RESET_STOP;
+    RESET_GLOBAL;
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+    while (!GET_STOP) {
+        pause();
+    }
+    return (GET_GLOBAL);
+}
+
+static void catch_signal(int signum)
+{
+    if (signum == SIGUSR1)
+        SET_GLOBAL(GET_GLOBAL + 1);
+    if (signum == SIGUSR2)
+        SET_STOP;
+}
