47aef340ad9e1f527399b87856c4a7c00a5a9619
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-22 16:21:17 +0200

    [UPDATE] #INCLUDE# {navy.h} Adding new player entry function prototype.

--- include/navy.h
+++ include/navy.h
@@ -21,7 +21,7 @@ typedef struct navy_s {
     char **map;
     char **enemy_map;
     uint32_t pid;
-    uint32_t enememy_pid;
+    uint32_t enemy_pid;
     uint8_t player_id;
 } navy_t;
 
@@ -37,6 +37,14 @@ int my_atoi(char const * const str);
 //<===============================================>
 // map_parser folder.
 char **parser_entry(char *map_path);
+
+//<===============================================>
+// player_one_entry.c.
+int player_entry(char *str_pid, char *map_path);
+
+//<===============================================>
+// create_map.c.
+char **create_map(void);
 
 //<===============================================>
 // player_one_entry.c.
