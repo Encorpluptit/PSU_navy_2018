a05347773d7f38879fe9ec899a6fe647f7511ab0
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-22 16:23:32 +0200

    [ADD] #SRC# {+player_entry} Adding player entry function for managing starting connection between players.

---
+++ src/player_entry.c
@@ -0,0 +1,134 @@
+/*
+** EPITECH PROJECT, 2019
+** Navy.
+** File description:
+** Source file for player entry in the prgrm.
+*/
+
+#include <signal.h>
+#include "essentials.h"
+#include "navy.h"
+#include "global.h"
+#include "printf.h"
+
+static int connect_players(navy_t *navy, int str_pid);
+static void connect(int signum, siginfo_t *si, void *ucontext);
+static int make_connection(uint8_t player_id, int str_pid);
+
+int player_entry(char *str_pid, char *map_path)
+{
+    pid_t pid = getpid();
+    int enemy_pid = -1;
+    navy_t navy = (navy_t) {.map = parser_entry(map_path), .pid = pid,
+    .enemy_map = create_map(), .player_id = (!str_pid) ? SIGUSR1 : SIGUSR2};
+
+    if (!(navy.map) || !(navy.enemy_map) ||
+        (str_pid && (my_str_isnum(str_pid) == ERROR)))
+        return ERROR; //return free;
+    my_printf("my_pid:  %d\n", pid);
+    if (navy.player_id == SIGUSR1)
+        connect_players(&navy, 0u);
+    else
+        connect_players(&navy, my_atoi(str_pid));
+}
+
+static int connect_players(navy_t *navy, int str_pid)
+{
+    struct sigaction sa = (struct sigaction) {.sa_sigaction = connect, .sa_flags = SA_SIGINFO};
+    pid_t enemy_pid = -1;
+
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+    while (make_connection(navy->player_id, str_pid) != SUCCESS);
+}
+
+static void connect(int signum, siginfo_t *si, void *ucontext)
+{
+    if (signum == SIGUSR1) {
+        my_printf("successfully connected\n");
+        SET_GLOBAL(si->si_pid);
+        return;
+    }
+    if (signum == SIGUSR2) {
+        my_printf("enemy connected\n");
+        SET_GLOBAL(si->si_pid);
+        return;
+    }
+}
+
+static int make_connection(uint8_t player_id, int str_pid)
+{
+    pid_t enemy_pid = 0;
+
+    if (player_id == SIGUSR1)
+        my_printf("waiting for enemy connection...\n\n");
+    else
+        kill(str_pid, SIGUSR2);
+    pause();
+    if ((enemy_pid = GET_GLOBAL)) {
+        if (player_id == SIGUSR1)
+            kill(enemy_pid, SIGUSR1);
+        return SUCCESS;
+    }
+    return ERROR;
+}
+//P1
+/*     my_printf("waiting for enemy connection...\n\n"); */
+/*     while (1) { */
+/*         pause(); */
+/*         if ((enemy_pid = GET_GLOBAL)) { */
+/*             kill(enemy_pid, SIGUSR1); */
+/*             break; */
+/*         } */
+/*     } */
+
+//P2
+/*     while (1) { */
+/*         kill(ennemy_pid, SIGUSR2); */
+/*         pause(); */
+/*         if ((enemy_pid = GET_GLOBAL)) { */
+/*             break; */
+/*         } */
+/*     } */
+
+/* static int connect_to_player_one(int ennemy_pid) */
+/* { */
+/*     struct sigaction sa; */
+/*     pid_t enemy_pid = -1; */
+
+/*     sa.sa_sigaction = make_connection; */
+/*     sa.sa_flags = SA_SIGINFO; */
+/* //    sigemptyset(&sa.sa_mask); */
+/*     sigaction(SIGUSR1, &sa, NULL); */
+/*     sigaction(SIGUSR2, &sa, NULL); */
+/*     while (1) { */
+/*         kill(ennemy_pid, SIGUSR2); */
+/*         pause(); */
+/*         if ((enemy_pid = GET_GLOBAL)) { */
+/*             break; */
+/*         } */
+/*     } */
+/*     return enemy_pid; */
+/* } */
+
+/* static int wait_for_player_two(void) */
+/* { */
+/*     struct sigaction sa; */
+/*     pid_t enemy_pid = -1; */
+
+/*     sa.sa_sigaction = make_connection; */
+/*     sa.sa_flags = SA_SIGINFO; */
+/*     sigemptyset(&sa.sa_mask); */
+/*     sigaction(SIGUSR1, &sa, NULL); */
+/*     sigaction(SIGUSR2, &sa, NULL); */
+/*     my_printf("waiting for enemy connection...\n\n"); */
+/*     while (1) { */
+/*         pause(); */
+/*         if ((enemy_pid = GET_GLOBAL)) { */
+/*             kill(enemy_pid, SIGUSR1); */
+/*             break; */
+/*         } */
+/*     } */
+/*     return enemy_pid; */
+/* } */
