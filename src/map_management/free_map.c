1b520c446509ae17b70f73104e22d8e9856446e4
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-23 00:42:30 +0200

    [UPDATE] #SRC/MAP_MANAGEMENT# {free_map} Adding free board function for free both map of the board.

--- src/map_management/free_map.c
+++ src/map_management/free_map.c
@@ -11,8 +11,17 @@
 
 int free_map(char **map)
 {
+    if (!map)
+        return SUCCESS;
     for (uint8_t i = 0; i < MAP_SIZE; ++i)
         free(*(map + i));
     free(map);
     return SUCCESS;
+}
+
+int free_board(char **map, char **enemy_map, int return_code)
+{
+    free(map);
+    free(enemy_map);
+    return return_code;
 }
