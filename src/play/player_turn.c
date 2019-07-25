36df7fa8f5f94eac04f8e851b3d919c6fedd3233
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-25 16:27:48 +0200

    [UPDATE] #SRC/PLAY# {player_turn.c} Adding automated tests instead of user entry.

--- src/play/player_turn.c
+++ src/play/player_turn.c
@@ -17,46 +17,83 @@ static void request_position(pos_t pos, navy_t *navy);
 
 int test(navy_t *navy, game_t *game, int turn)
 {
-    pos_t pos[] = {
-        {.col = 0, .line = 2},
-        {.col = 1, .line = 2},
-        {.col = 3, .line = 3},
-        {.col = 3, .line = 4},
-        {.col = 3, .line = 5},
-        {.col = 4, .line = 1},
-        {.col = 5, .line = 1},
-        {.col = 6, .line = 1},
-        {.col = 7, .line = 1},
-        {.col = 6, .line = 3},
-        {.col = 6, .line = 4},
-        {.col = 6, .line = 5},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 6},
-        {.col = 6, .line = 7}
-    };
+    /* pos_t pos[] = { */
+    /*     {.col = 0, .line = 2}, */
+    /*     {.col = 0, .line = 1}, */
+    /*     {.col = 1, .line = 2}, */
+    /*     {.col = 3, .line = 3}, */
+    /*     {.col = 3, .line = 4}, */
+    /*     {.col = 3, .line = 5}, */
+    /*     {.col = 4, .line = 1}, */
+    /*     {.col = 5, .line = 1}, */
+    /*     {.col = 6, .line = 1}, */
+    /*     {.col = 7, .line = 1}, */
+    /*     {.col = 6, .line = 3}, */
+    /*     {.col = 6, .line = 4}, */
+    /*     {.col = 6, .line = 5}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 6}, */
+    /*     {.col = 6, .line = 7} */
+    /* }; */
 
-    if (pos && ((turn + 1) >= sizeof(pos) / sizeof(pos[0])))
+    /* send_info(navy->enemy_pid, HIT); */
+    /* if (navy->player_id == SIGUSR2 && !turn) { */
+    /*     my_printf("attack:  %c%c\n", 'A', '2'); */
+    /*     request_position((pos_t) {.col = 0, .line = 0}, navy); */
+    /*     hit_target(game, (pos_t) {.col = 0, .line = 0}, navy); */
+    /* } else { */
+    /*     my_printf("attack:  %c%c\n", (pos[turn]).col + 'A', (pos[turn]).line + '1'); */
+    /*     request_position(pos[turn], navy); */
+    /*     hit_target(game, pos[turn], navy); */
+    /* } */
+    /* return SUCCESS; */
+
+    static uint8_t col = 0;
+    static uint8_t line = 0;
+    static uint32_t stop = 0;
+
+    if (col > 7) {
+        col = 0;
+        line ++;
+    }
+    if (line >= 7 && col >= 6) {
+        stop++;
+        col = 0;
+        line = 0;
+    }
+    my_printf("STOP = %d\n", stop);
+    if (stop > 400) {
+        my_printf("STOP = %d\n", stop);
         return ERROR;
-//    usleep(100000);
+    }
+    pos_t pos = (pos_t) {.line = line, .col = col};
+
+    /* if (pos && ((turn + 1) >= sizeof(pos) / sizeof(pos[0]))) */
+    /*     return ERROR; */
+    usleep(2000);
     send_info(navy->enemy_pid, HIT);
-    if (navy->player_id == SIGUSR1 && !turn) {
+    if (navy->player_id == SIGUSR2 && !turn) {
         my_printf("attack:  %c%c\n", 'A', '2');
+        usleep(2000);
         request_position((pos_t) {.col = 0, .line = 0}, navy);
         hit_target(game, (pos_t) {.col = 0, .line = 0}, navy);
     } else {
-        my_printf("attack:  %c%c\n", (pos[turn]).col + 'A', (pos[turn]).line + '1');
-        request_position(pos[turn], navy);
-        hit_target(game, pos[turn], navy);
+        my_printf("attack:  %c%c\n", (pos).col + 'A', (pos).line + '1');
+        usleep(2000);
+        request_position(pos, navy);
+        hit_target(game, pos, navy);
     }
+    col++;
+    return SUCCESS;
 }
 
 uint8_t player_turn(navy_t *navy, game_t *game)
@@ -65,23 +102,22 @@ uint8_t player_turn(navy_t *navy, game_t *game)
 
     if (navy->player_id == SIGUSR1)
         print_board(navy->map, navy->enemy_map);
-
+    usleep(2000);
     static int turn = 0;
-
     if (test(navy, game,turn) != ERROR) {
         turn += 1;
     }
     else {
-        usleep(100000);
+        usleep(2000);
         if ((pos = get_user_input(navy, game)).line == ERROR)
-            return ERROR;
+            return QUIT;
         request_position(pos, navy);
         hit_target(game, pos, navy);
         turn += 1;
     }
 
     /* if ((pos = get_user_input(navy, game)).line == ERROR) */
-    /*     return ERROR; */
+    /*         return QUIT; */
     /* request_position(pos, navy); */
     /* hit_target(game, pos, navy); */
 
@@ -91,6 +127,7 @@ uint8_t player_turn(navy_t *navy, game_t *game)
 
 static void request_position(pos_t pos, navy_t *navy)
 {
+    usleep(2000);
     send_info(navy->enemy_pid, pos.col);
     send_info(navy->enemy_pid, pos.line);
 }
@@ -115,36 +152,21 @@ static uint8_t hit_target(game_t *game, pos_t pos, navy_t *navy)
     return SUCCESS;
 }
 
-static void signal_handler(int signum)
-{
-    if (signum == SIGUSR1 || signum == SIGUSR2)
-        return;
-}
-
 static pos_t get_user_input(navy_t *navy, game_t *game)
 {
     char *input = NULL;
 
-    /* my_printf("passe dans get_user_input\n"); */
-    /* signal(SIGUSR1, signal_handler); */
-    /* signal(SIGUSR2, signal_handler); */
     do {
         my_printf("attack:  ");
         input = get_next_line(0u);
-        /* if (!input) */
-        /*     continue; */
-        /* my_printf("inp: %s\n", input ? input : "false"); */
-        /* usleep(10000); */
         if (!input) {
-            my_printf("passe dans no input\n");
             game->play = false;
             print_board(navy->map, navy->enemy_map);
-            send_info(navy->enemy_pid, STOP_GAME);
+            send_info(navy->enemy_pid, QUIT);
             return ((pos_t) {.line = ERROR, .col = ERROR});
-        /* } */
-        } else
-            send_info(navy->enemy_pid, HIT);
+        }
     } while (!check_user_synthax(input));
+    send_info(navy->enemy_pid, MISSED);
     return ((pos_t) {.col = *input - 'A', .line = *(input + 1) - '1'});
 }
 
@@ -156,9 +178,6 @@ static bool check_user_synthax(char *input)
         my_printf("wrong position str\n");
         return false;
     }
-    /* my_printf("%c\t%c\n", *input, *(input + 1)); */
-    /* my_printf("letter: %s", IS_LETTER(*input) ? "true" : "false"); */
-    /* my_printf("number: %s", IS_NUMBER(*(input + 1)) ? "true" : "false"); */
     if (IS_LETTER(*input) && IS_NUMBER(*(input + 1)))
         return true;
     else {
