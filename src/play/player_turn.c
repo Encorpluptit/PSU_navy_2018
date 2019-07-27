daba9e73d18fd12081824c61818158a8769579d2
BERNARD Damien <damien.bernard@epitech.eu>
2019-07-27 10:47:55 +0200

    [UPDATE] #FTESTS# Updating test for monster test (2000 map turns).

--- src/play/player_turn.c
+++ src/play/player_turn.c
@@ -15,48 +15,8 @@ static pos_t get_user_input(navy_t *navy, game_t *game);
 static bool check_user_synthax(char *input);
 static void request_position(pos_t pos, navy_t *navy);
 
-int test(navy_t *navy, game_t *game, int turn)
+int test(navy_t *navy, game_t *game)
 {
-    /* pos_t pos[] = { */
-    /*     {.col = 0, .line = 2}, */
-    /*     {.col = 0, .line = 1}, */
-    /*     {.col = 1, .line = 2}, */
-    /*     {.col = 3, .line = 3}, */
-    /*     {.col = 3, .line = 4}, */
-    /*     {.col = 3, .line = 5}, */
-    /*     {.col = 4, .line = 1}, */
-    /*     {.col = 5, .line = 1}, */
-    /*     {.col = 6, .line = 1}, */
-    /*     {.col = 7, .line = 1}, */
-    /*     {.col = 6, .line = 3}, */
-    /*     {.col = 6, .line = 4}, */
-    /*     {.col = 6, .line = 5}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 6}, */
-    /*     {.col = 6, .line = 7} */
-    /* }; */
-
-    /* send_info(navy->enemy_pid, HIT); */
-    /* if (navy->player_id == SIGUSR2 && !turn) { */
-    /*     my_printf("attack:  %c%c\n", 'A', '2'); */
-    /*     request_position((pos_t) {.col = 0, .line = 0}, navy); */
-    /*     hit_target(game, (pos_t) {.col = 0, .line = 0}, navy); */
-    /* } else { */
-    /*     my_printf("attack:  %c%c\n", (pos[turn]).col + 'A', (pos[turn]).line + '1'); */
-    /*     request_position(pos[turn], navy); */
-    /*     hit_target(game, pos[turn], navy); */
-    /* } */
-    /* return SUCCESS; */
-
     static uint8_t col = 0;
     static uint8_t line = 0;
     static uint32_t stop = 0;
@@ -73,49 +33,28 @@ int test(navy_t *navy, game_t *game, int turn)
         line = 0;
     }
     my_printf("STOP = %d\n", stop);
-    if (stop > 400) {
-        my_printf("STOP = %d\n", stop);
-        return ERROR;
+    if (stop > 2000) {
+        col = 1;
+        line = 7;
     }
     pos_t pos = (pos_t) {.line = line, .col = col};
 
-    /* if (pos && ((turn + 1) >= sizeof(pos) / sizeof(pos[0]))) */
-    /*     return ERROR; */
     usleep(2000);
     send_info(navy->enemy_pid, HIT);
-    if (navy->player_id == SIGUSR2 && !turn) {
-        my_printf("attack:  %c%c\n", 'A', '2');
-        usleep(2000);
-        request_position((pos_t) {.col = 0, .line = 0}, navy);
-        hit_target(game, (pos_t) {.col = 0, .line = 0}, navy);
-    } else {
-        my_printf("attack:  %c%c\n", (pos).col + 'A', (pos).line + '1');
-        usleep(2000);
-        request_position(pos, navy);
-        hit_target(game, pos, navy);
-    }
+    my_printf("attack:  %c%c\n", (pos).col + 'A', (pos).line + '1');
+    usleep(2000);
+    request_position(pos, navy);
+    hit_target(game, pos, navy);
     col++;
     return SUCCESS;
 }
 
 uint8_t player_turn(navy_t *navy, game_t *game)
 {
-    pos_t pos;
-
     if (navy->player_id == SIGUSR1)
         print_board(navy->map, navy->enemy_map);
     usleep(4000);
-    static int turn = 0;
-    if (test(navy, game, turn) != ERROR) {
-        turn += 1;
-    }
-    else {
-        usleep(2000);
-        if ((pos = get_user_input(navy, game)).line == ERROR)
-            return QUIT;
-    }
-    request_position(pos, navy);
-    hit_target(game, pos, navy);
+    test(navy, game);
     game->turn = false;
     return game->win;
 }
